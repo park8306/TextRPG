9552fb17c1b3318aa4bb5de14fe609ac2493e41b
park8306 <kosaf0804@gmail.com>
2025-01-31 20:40:25 +0900

    초기 커밋.

---
+++ TextRPG/Player.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TextRPG
+{
+    // 레벨 / 이름 / 직업 / 공격력 / 방어력 / 체력 / Gold
+    public class Player
+    {
+        public int Level { get; set; }
+        public string Name { get; set; }
+        public string Chad { get; set; }
+        public int Att {  get; set; }
+        public int Def { get; set; }
+        public int MaxHP { get; set; }
+        public int CurHP {  get; set; }
+        public int Gold {  get; set; }
+
+        public Player(int level = 1, string name = "", string chad = "전사", int att = 10, int def = 5, int maxHP = 100, int gold = 1500)
+        {
+            Level = level;
+            Name = name;
+            Chad = chad;
+            Att = att;
+            Def = def;
+            MaxHP = maxHP;
+            CurHP = MaxHP;
+            Gold = gold;
+        }
+
+        public override string ToString()
+        {
+            string level = Level.ToString().Length > 2 ? Level.ToString() : "0" + Level.ToString();
+
+            string info = $"Lv. {level}\nChad ( {Chad} )\n공격력 : {Att}\n방어력 : {Def}\n체력 : {CurHP} / {MaxHP}\nGold : {Gold} G";
+            return info;
+        }
+    }
+}
---
+++ TextRPG/Program.cs
@@ -0,0 +1,88 @@
+﻿using System.ComponentModel.Design;
+
+namespace TextRPG
+{
+    internal class Program
+    {
+        enum GameState
+        {
+            Info = 1,
+            Inventory,
+            Shop,
+            Dungeon,
+            Rest,
+            Count
+        }
+
+        static void Main(string[] args)
+        {
+            Player player = new Player();
+
+            Console.WriteLine("Text RPG에 입장하신 것을 환영합니다.");
+            Console.WriteLine("이름을 입력해주세요!");
+            Console.Write(">>");
+            player.Name = Console.ReadLine();
+
+            while (true)
+            {
+                Console.Clear();
+                Console.WriteLine("TextRPG");
+                Console.WriteLine();
+
+                Console.WriteLine("1. 상태 보기");
+                Console.WriteLine("2. 인벤토리");
+                Console.WriteLine("3. 상점");
+                Console.WriteLine("4. 던전입장");
+                Console.WriteLine("5. 휴식하기");
+                Console.WriteLine();
+
+                Console.WriteLine("원하시는 행동을 입력해주세요.");
+                Console.Write(">>");
+
+                int select;
+                if ((int.TryParse(Console.ReadLine(), out select))
+                    && select > 0
+                    && select <= (int)GameState.Count)
+                {
+                    switch (select)
+                    {
+                        case 1:
+                            while(true)
+                            {
+                                Console.Clear();
+                                Console.WriteLine("상태 보기");
+                                Console.WriteLine("캐릭터 정보");
+                                Console.WriteLine();
+
+                                Console.WriteLine(player.ToString());
+                                Console.WriteLine();
+
+                                Console.WriteLine("0. 나가기");
+                                Console.WriteLine();
+
+                                Console.WriteLine("원하시는 행동을 입력해주세요.");
+                                Console.Write(">>");
+                                if ((int.TryParse(Console.ReadLine(), out select)) && select == 0)
+                                {
+                                    break;
+                                }
+                                else
+                                {
+                                    Console.WriteLine("잘못된 입력입니다.");
+                                    Thread.Sleep(1000);
+                                }
+                            }
+                            break;
+                        default:
+                            break;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("잘못된 입력입니다.");
+                    Thread.Sleep(1000);
+                }
+            }
+        }
+    }
+}
